929be911cdaef45e425d321ab071d638ce028fe9
2277 <38501234+2277@users.noreply.github.com>
2018-11-11 07:38:05 +0000

    Change /tpa and /tpahere notifications to use JSON Text Components

--- Essentials/src/com/earth2me/essentials/commands/Commandtpa.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandtpa.java
@@ -1,60 +1,73 @@
 package com.earth2me.essentials.commands;
 
-import com.earth2me.essentials.User;
-import org.bukkit.Server;
+import static com.earth2me.essentials.I18n.tl;
 
 import java.util.Collections;
 import java.util.List;
 
-import static com.earth2me.essentials.I18n.tl;
+import org.bukkit.Server;
 
+import com.earth2me.essentials.User;
+
+import mkremins.fanciful.FancyMessage;
 
 public class Commandtpa extends EssentialsCommand {
-    public Commandtpa() {
-        super("tpa");
-    }
+	public Commandtpa() {
+		super("tpa");
+	}
 
-    @Override
-    public void run(Server server, User user, String commandLabel, String[] args) throws Exception {
-        if (args.length < 1) {
-            throw new NotEnoughArgumentsException();
-        }
+	@Override
+	public void run(Server server, User user, String commandLabel, String[] args) throws Exception {
+		if (args.length < 1) { throw new NotEnoughArgumentsException(); }
 
-        User player = getPlayer(server, user, args, 0);
-        if (user.getName().equalsIgnoreCase(player.getName())) {
-            throw new NotEnoughArgumentsException();
-        }
-        if (!player.isTeleportEnabled()) {
-            throw new Exception(tl("teleportDisabled", player.getDisplayName()));
-        }
-        if (user.getWorld() != player.getWorld() && ess.getSettings().isWorldTeleportPermissions() && !user.isAuthorized("essentials.worlds." + player.getWorld().getName())) {
-            throw new Exception(tl("noPerm", "essentials.worlds." + player.getWorld().getName()));
-        }
-        // Don't let sender request teleport twice to the same player.
-        if (user.getConfigUUID().equals(player.getTeleportRequest()) && player.hasOutstandingTeleportRequest() // Check timeout
-            && player.isTpRequestHere() == false) { // Make sure the last teleport request was actually tpa and not tpahere
-            throw new Exception(tl("requestSentAlready", player.getDisplayName()));
-        }
+		User player = getPlayer(server, user, args, 0);
+		if (user.getName().equalsIgnoreCase(player.getName())) { throw new NotEnoughArgumentsException(); }
+		if (!player.isTeleportEnabled()) { throw new Exception(tl("teleportDisabled", player.getDisplayName())); }
+		if (user.getWorld() != player.getWorld() && ess.getSettings().isWorldTeleportPermissions()
+				&& !user.isAuthorized("essentials.worlds." + player.getWorld().getName())) { throw new Exception(
+						tl("noPerm", "essentials.worlds." + player.getWorld().getName())); }
+		// Don't let sender request teleport twice to the same player.
+		if (user.getConfigUUID().equals(player.getTeleportRequest()) && player.hasOutstandingTeleportRequest() // Check
+																												// timeout
+				&& player.isTpRequestHere() == false) { // Make sure the last teleport request was actually tpa and not
+														// tpahere
+			throw new Exception(tl("requestSentAlready", player.getDisplayName()));
+		}
 
-        if (!player.isIgnoredPlayer(user)) {
-            player.requestTeleport(user, false);
-            player.sendMessage(tl("teleportRequest", user.getDisplayName()));
-            player.sendMessage(tl("typeTpaccept"));
-            player.sendMessage(tl("typeTpdeny"));
-            if (ess.getSettings().getTpaAcceptCancellation() != 0) {
-                player.sendMessage(tl("teleportRequestTimeoutInfo", ess.getSettings().getTpaAcceptCancellation()));
-            }
-        }
-        user.sendMessage(tl("requestSent", player.getDisplayName()));
-        user.sendMessage(tl("typeTpacancel"));
-    }
+		if (!player.isIgnoredPlayer(user)) {
+			player.requestTeleport(user, false);
 
-    @Override
-    protected List<String> getTabCompleteOptions(Server server, User user, String commandLabel, String[] args) {
-        if (args.length == 1) {
-            return getPlayers(server, user);
-        } else {
-            return Collections.emptyList();
-        }
-    }
+			FancyMessage message = new FancyMessage(tl("teleportRequest", user.getDisplayName()));
+
+			if (ess.getSettings().getTpaAcceptCancellation() != 0) {
+				message.tooltip(tl("teleportRequestTimeoutInfo", ess.getSettings().getTpaAcceptCancellation()));
+			}
+
+			message.then(tl("clickTpaccept"))
+					.tooltip(tl("tooltipTpaccept"))
+					.command("/tpaccept")
+					.then(tl("clickTpdeny"))
+					.tooltip(tl("tooltipTpdeny"))
+					.command("/tpdeny");
+			message.send(player.getBase());
+
+//            player.sendMessage(tl("teleportRequest", user.getDisplayName()));
+//            player.sendMessage(tl("typeTpaccept"));
+//            player.sendMessage(tl("typeTpdeny"));
+//            if (ess.getSettings().getTpaAcceptCancellation() != 0) {
+//                player.sendMessage(tl("teleportRequestTimeoutInfo", ess.getSettings().getTpaAcceptCancellation()));
+//            }
+		}
+		user.sendMessage(tl("requestSent", player.getDisplayName()));
+		user.sendMessage(tl("typeTpacancel"));
+	}
+
+	@Override
+	protected List<String> getTabCompleteOptions(Server server, User user, String commandLabel, String[] args) {
+		if (args.length == 1) {
+			return getPlayers(server, user);
+		} else {
+			return Collections.emptyList();
+		}
+	}
 }
--- Essentials/src/com/earth2me/essentials/commands/Commandtpahere.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandtpahere.java
@@ -1,59 +1,74 @@
 package com.earth2me.essentials.commands;
 
-import com.earth2me.essentials.User;
-import org.bukkit.Server;
+import static com.earth2me.essentials.I18n.tl;
 
 import java.util.Collections;
 import java.util.List;
 
-import static com.earth2me.essentials.I18n.tl;
+import org.bukkit.Server;
 
+import com.earth2me.essentials.User;
+
+import mkremins.fanciful.FancyMessage;
 
 public class Commandtpahere extends EssentialsCommand {
-    public Commandtpahere() {
-        super("tpahere");
-    }
+	public Commandtpahere() {
+		super("tpahere");
+	}
 
-    @Override
-    public void run(final Server server, final User user, final String commandLabel, final String[] args) throws Exception {
-        if (args.length < 1) {
-            throw new NotEnoughArgumentsException();
-        }
+	@Override
+	public void run(final Server server, final User user, final String commandLabel, final String[] args)
+			throws Exception {
+		if (args.length < 1) { throw new NotEnoughArgumentsException(); }
 
-        final User player = getPlayer(server, user, args, 0);
-        if (user.getName().equalsIgnoreCase(player.getName())) {
-            throw new NotEnoughArgumentsException();
-        }
-        if (!player.isTeleportEnabled()) {
-            throw new Exception(tl("teleportDisabled", player.getDisplayName()));
-        }
-        if (user.getWorld() != player.getWorld() && ess.getSettings().isWorldTeleportPermissions() && !user.isAuthorized("essentials.worlds." + user.getWorld().getName())) {
-            throw new Exception(tl("noPerm", "essentials.worlds." + user.getWorld().getName()));
-        }
-        // Don't let sender request teleport twice to the same player.
-        if (user.getConfigUUID().equals(player.getTeleportRequest()) && player.hasOutstandingTeleportRequest() // Check timeout
-            && player.isTpRequestHere() == true) { // Make sure the last teleport request was actually tpahere and not tpa
-            throw new Exception(tl("requestSentAlready", player.getDisplayName()));
-        }
-        if (!player.isIgnoredPlayer(user)) {
-            player.requestTeleport(user, true);
-            player.sendMessage(tl("teleportHereRequest", user.getDisplayName()));
-            player.sendMessage(tl("typeTpaccept"));
-            player.sendMessage(tl("typeTpdeny"));
-            if (ess.getSettings().getTpaAcceptCancellation() != 0) {
-                player.sendMessage(tl("teleportRequestTimeoutInfo", ess.getSettings().getTpaAcceptCancellation()));
-            }
-        }
-        user.sendMessage(tl("requestSent", player.getDisplayName()));
-        user.sendMessage(tl("typeTpacancel"));
-    }
+		final User player = getPlayer(server, user, args, 0);
+		if (user.getName().equalsIgnoreCase(player.getName())) { throw new NotEnoughArgumentsException(); }
+		if (!player.isTeleportEnabled()) { throw new Exception(tl("teleportDisabled", player.getDisplayName())); }
+		if (user.getWorld() != player.getWorld() && ess.getSettings().isWorldTeleportPermissions()
+				&& !user.isAuthorized("essentials.worlds." + user.getWorld().getName())) { throw new Exception(
+						tl("noPerm", "essentials.worlds." + user.getWorld().getName())); }
+		// Don't let sender request teleport twice to the same player.
+		if (user.getConfigUUID().equals(player.getTeleportRequest()) && player.hasOutstandingTeleportRequest() // Check
+																												// timeout
+				&& player.isTpRequestHere() == true) { // Make sure the last teleport request was actually tpahere and
+														// not tpa
+			throw new Exception(tl("requestSentAlready", player.getDisplayName()));
+		}
+		if (!player.isIgnoredPlayer(user)) {
+			player.requestTeleport(user, true);
 
-    @Override
-    protected List<String> getTabCompleteOptions(Server server, User user, String commandLabel, String[] args) {
-        if (args.length == 1) {
-            return getPlayers(server, user);
-        } else {
-            return Collections.emptyList();
-        }
-    }
+			FancyMessage message = new FancyMessage(tl("teleportHereRequest", user.getDisplayName()));
+
+			if (ess.getSettings().getTpaAcceptCancellation() != 0) {
+				message.tooltip(tl("teleportRequestTimeoutInfo", ess.getSettings().getTpaAcceptCancellation()));
+			}
+
+			message.then(tl("clickTpaccept"))
+					.tooltip(tl("tooltipTpaccept"))
+					.command("/tpaccept")
+					.then(tl("clickTpdeny"))
+					.tooltip(tl("tooltipTpdeny"))
+					.command("/tpdeny");
+			message.send(player.getBase());
+
+//			player.sendMessage(tl("teleportHereRequest", user.getDisplayName()));
+//			player.sendMessage(tl("typeTpaccept"));
+//			player.sendMessage(tl("typeTpdeny"));
+//			if (ess.getSettings().getTpaAcceptCancellation() != 0) {
+//				player.sendMessage(tl("teleportRequestTimeoutInfo", ess.getSettings().getTpaAcceptCancellation()));
+//			}
+
+		}
+		user.sendMessage(tl("requestSent", player.getDisplayName()));
+		user.sendMessage(tl("typeTpacancel"));
+	}
+
+	@Override
+	protected List<String> getTabCompleteOptions(Server server, User user, String commandLabel, String[] args) {
+		if (args.length == 1) {
+			return getPlayers(server, user);
+		} else {
+			return Collections.emptyList();
+		}
+	}
 }
