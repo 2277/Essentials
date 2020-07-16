95c8c4567e0f914c97e984b7605c5bd96443a802
Callum <38501234+calumari@users.noreply.github.com>
2020-07-16 21:37:40 +0100

    Add block logging to editsign command

--- Essentials/src/com/earth2me/essentials/commands/Commandeditsign.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandeditsign.java
@@ -3,12 +3,15 @@ package com.earth2me.essentials.commands;
 import com.earth2me.essentials.User;
 import com.earth2me.essentials.utils.FormatUtil;
 import com.earth2me.essentials.utils.NumberUtil;
+import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import org.bukkit.ChatColor;
 import org.bukkit.Server;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.event.block.SignChangeEvent;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -30,6 +33,7 @@ public class Commandeditsign extends EssentialsCommand {
             throw new Exception(tl("editsignCommandTarget"));
         }
         Sign sign = (Sign) target.getState();
+        String[] lines = sign.getLines();
         try {
             if (args[0].equalsIgnoreCase("set") && args.length > 2) {
                 int line = Integer.parseInt(args[1]) - 1;
@@ -37,19 +41,41 @@ public class Commandeditsign extends EssentialsCommand {
                 if (ChatColor.stripColor(text).length() > 15 && !user.isAuthorized("essentials.editsign.unlimited")) {
                     throw new Exception(tl("editsignCommandLimit"));
                 }
+                lines[line] = text;
+                SignChangeEvent event = new SignChangeEvent(target, user.getBase(), lines);
+                server.getPluginManager().callEvent(event);
+                if (event.isCancelled()) {
+                    user.sendMessage(tl("editsignCommandCancelled"));
+                    return;
+                }
+                text = Strings.nullToEmpty(event.getLine(line));
                 sign.setLine(line, text);
                 sign.update();
                 user.sendMessage(tl("editsignCommandSetSuccess", line + 1, text));
             } else if (args[0].equalsIgnoreCase("clear")) {
                 if (args.length == 1) {
+                    Arrays.fill(lines, "");
+                    SignChangeEvent event = new SignChangeEvent(target, user.getBase(), lines);
+                    server.getPluginManager().callEvent(event);
+                    if (event.isCancelled()) {
+                        user.sendMessage(tl("editsignCommandCancelled"));
+                        return;
+                    }
                     for (int i = 0; i < 4; i++) { // A whole one line of line savings!
-                        sign.setLine(i, "");
+                        sign.setLine(i, Strings.nullToEmpty(event.getLine(i)));
                     }
                     sign.update();
                     user.sendMessage(tl("editsignCommandClear"));
                 } else {
                     int line = Integer.parseInt(args[1]) - 1;
-                    sign.setLine(line, "");
+                    lines[line] = "";
+                    SignChangeEvent event = new SignChangeEvent(target, user.getBase(), lines);
+                    server.getPluginManager().callEvent(event);
+                    if (event.isCancelled()) {
+                        user.sendMessage(tl("editsignCommandCancelled"));
+                        return;
+                    }
+                    sign.setLine(line, Strings.nullToEmpty(event.getLine(line)));
                     sign.update();
                     user.sendMessage(tl("editsignCommandClearLine", line + 1));
                 }
