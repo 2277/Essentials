c93d07d00c6e24029ba25eb2914fe6d0bc34932f
Callum <38501234+calumari@users.noreply.github.com>
2020-08-09 14:55:45 +0100

    Remove pitch/yaw arguments from tpchunk command

--- Essentials/src/com/earth2me/essentials/commands/Commandtpchunk.java
+++ Essentials/src/com/earth2me/essentials/commands/Commandtpchunk.java
@@ -3,7 +3,6 @@ package com.earth2me.essentials.commands;
 import com.earth2me.essentials.CommandSource;
 import com.earth2me.essentials.Trade;
 import com.earth2me.essentials.User;
-import com.earth2me.essentials.utils.FloatUtil;
 import com.google.common.collect.Lists;
 import org.bukkit.Location;
 import org.bukkit.Server;
@@ -28,7 +27,7 @@ public class Commandtpchunk extends EssentialsCommand {
         }
 
         final int x = args[0].startsWith("~") ? (user.getLocation().getBlockX() >> 4) + (args[0].length() > 1 ? Integer.parseInt(args[0].substring(1)) : 0) : Integer.parseInt(args[0]);
-        final int z = args[1].startsWith("~") ? (user.getLocation().getBlockZ() >> 4) + (args[2].length() > 1 ? Integer.parseInt(args[2].substring(1)) : 0) : Integer.parseInt(args[2]);
+        final int z = args[1].startsWith("~") ? (user.getLocation().getBlockZ() >> 4) + (args[1].length() > 1 ? Integer.parseInt(args[1].substring(1)) : 0) : Integer.parseInt(args[1]);
         final Location loc = new Location(user.getWorld(), x << 4, user.getLocation().getY(), z << 4, user.getLocation().getYaw(), user.getLocation().getPitch());
         if (args.length == 3) {
             World w = ess.getWorld(args[2]);
@@ -37,18 +36,7 @@ public class Commandtpchunk extends EssentialsCommand {
             }
             loc.setWorld(w);
         }
-        if (args.length > 3) {
-            loc.setYaw((FloatUtil.parseFloat(args[2]) + 360) % 360);
-            loc.setPitch(FloatUtil.parseFloat(args[3]));
-        }
-        if (args.length > 4) {
-            World w = ess.getWorld(args[4]);
-            if (user.getWorld() != w && ess.getSettings().isWorldTeleportPermissions() && !user.isAuthorized("essentials.worlds." + w.getName())) {
-                throw new Exception(tl("noPerm", "essentials.worlds." + w.getName()));
-            }
-            loc.setWorld(w);
-        }
-        if (x > 30000000 || z > 30000000 || x < -30000000 || z < -30000000) {
+        if (loc.getX() > 30000000 || loc.getZ() > 30000000 || loc.getX() < -30000000 || loc.getZ() < -30000000) {
             throw new NotEnoughArgumentsException(tl("teleportInvalidLocation"));
         }
         final Trade charge = new Trade(this.getName(), ess);
@@ -65,19 +53,12 @@ public class Commandtpchunk extends EssentialsCommand {
 
         User user = getPlayer(server, args, 0, true, false);
         final int x = args[1].startsWith("~") ? (user.getLocation().getBlockX() >> 4) + (args[0].length() > 1 ? Integer.parseInt(args[0].substring(1)) : 0) : Integer.parseInt(args[0]);
-        final int z = args[2].startsWith("~") ? (user.getLocation().getBlockZ() >> 4) + (args[2].length() > 1 ? Integer.parseInt(args[2].substring(1)) : 0) : Integer.parseInt(args[2]);
+        final int z = args[2].startsWith("~") ? (user.getLocation().getBlockZ() >> 4) + (args[1].length() > 1 ? Integer.parseInt(args[1].substring(1)) : 0) : Integer.parseInt(args[1]);
         final Location loc = new Location(user.getWorld(), x << 4, user.getLocation().getY(), z << 4, user.getLocation().getYaw(), user.getLocation().getPitch());
         if (args.length == 4) {
             loc.setWorld(ess.getWorld(args[3]));
         }
-        if (args.length > 4) {
-            loc.setYaw((FloatUtil.parseFloat(args[3]) + 360) % 360);
-            loc.setPitch(FloatUtil.parseFloat(args[4]));
-        }
-        if (args.length > 5) {
-            loc.setWorld(ess.getWorld(args[5]));
-        }
-        if (x > 30000000 || z > 30000000 || x < -30000000 || z < -30000000) {
+        if (loc.getX() > 30000000 || loc.getZ() > 30000000 || loc.getX() < -30000000 || loc.getZ() < -30000000) {
             throw new NotEnoughArgumentsException(tl("teleportInvalidLocation"));
         }
         sender.sendMessage(tl("teleporting", loc.getWorld().getName(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()));
@@ -89,17 +70,14 @@ public class Commandtpchunk extends EssentialsCommand {
     protected List<String> getTabCompleteOptions(Server server, User user, String commandLabel, String[] args) {
         if (args.length == 1 || args.length == 2) {
             return Lists.newArrayList("~0");
-        } else if (args.length == 3 || args.length == 4) {
-            return Lists.newArrayList("0");
-        } else if (args.length == 5) {
+        } else if (args.length == 3) {
             List<String> worlds = Lists.newArrayList();
             for (World world : server.getWorlds()) {
                 worlds.add(world.getName());
             }
             return worlds;
-        } else {
-            return Collections.emptyList();
         }
+        return Collections.emptyList();
     }
 
     @Override
@@ -108,16 +86,13 @@ public class Commandtpchunk extends EssentialsCommand {
             return getPlayers(server, sender);
         } else if (args.length == 2 || args.length == 3) {
             return Lists.newArrayList("~0");
-        } else if (args.length == 4 || args.length == 5) {
-            return Lists.newArrayList("0");
-        } else if (args.length == 6) {
+        } else if (args.length == 4) {
             List<String> worlds = Lists.newArrayList();
             for (World world : server.getWorlds()) {
                 worlds.add(world.getName());
             }
             return worlds;
-        } else {
-            return Collections.emptyList();
         }
+        return Collections.emptyList();
     }
 }
